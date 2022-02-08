e539793d0ff70cb2275418532a127b804b40610d
Arno <1215722993@qq.com>
2022-02-08 22:14:28 +0800

    Signed-off-by: Arno <1215722993@qq.com> OK

---
+++ Demo/medicalservice/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+html,
+body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.el-container {
+  height: 100%;
+}
+
+.el-container::-webkit-scrollbar,
+html::-webkit-scrollbar,
+body::-webkit-scrollbar {
+  display: none;
+}
+</style>
